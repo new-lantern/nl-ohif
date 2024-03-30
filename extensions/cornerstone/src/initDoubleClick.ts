dc88bd18b37880a8d5cb5ac83abb1a26bf7561b8
Daniel <schwartzray8@gmail.com>
2024-03-29 17:34:30 -0700

    disable double click while magic roi is activated

--- extensions/cornerstone/src/initDoubleClick.ts
+++ extensions/cornerstone/src/initDoubleClick.ts
@@ -1,6 +1,6 @@
 import { eventTarget, EVENTS } from '@cornerstonejs/core';
 import { Enums } from '@cornerstonejs/tools';
-import { CommandsManager, CustomizationService, Types } from '@ohif/core';
+import { CommandsManager, CustomizationService, ToolbarService, Types } from '@ohif/core';
 import { findNearbyToolData } from './utils/findNearbyToolData';
 
 const cs3DToolsEvents = Enums.Events;
@@ -36,14 +36,21 @@ function getDoubleClickEventName(evt: CustomEvent) {
 
 export type initDoubleClickArgs = {
   customizationService: CustomizationService;
+  toolbarService: ToolbarService;
   commandsManager: CommandsManager;
 };
 
-function initDoubleClick({ customizationService, commandsManager }: initDoubleClickArgs): void {
+function initDoubleClick({
+  customizationService,
+  toolbarService,
+  commandsManager,
+}: initDoubleClickArgs): void {
   const cornerstoneViewportHandleDoubleClick = (evt: CustomEvent) => {
+    const activeTools = toolbarService.getActiveTools();
+    console.log(activeTools);
     // Do not allow double click on a tool.
     const nearbyToolData = findNearbyToolData(commandsManager, evt);
-    if (nearbyToolData) {
+    if (nearbyToolData || activeTools.includes('MagicROI')) {
       return;
     }
 
