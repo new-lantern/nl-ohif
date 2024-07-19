c02a1627ddca28fb44abe8c0b5353ab4b2092697
m00n620 <gex6611@gmail.com>
2024-07-18 21:08:56 -0400

    remove transition

--- platform/ui/src/components/ViewportPane/ViewportPane.tsx
+++ platform/ui/src/components/ViewportPane/ViewportPane.tsx
@@ -98,7 +98,7 @@ function ViewportPane({
       onScroll={onInteractionHandler}
       onWheel={onInteractionHandler}
       className={classnames(
-        'group h-full w-full overflow-hidden rounded-md transition duration-300',
+        'group h-full w-full overflow-hidden rounded-md',
         {
           'border-primary-light border-2': isActive,
           'border-2 border-transparent': !isActive,
@@ -152,7 +152,7 @@ ViewportPane.propTypes = {
   originViewportId: PropTypes.string,
 };
 
-const noop = () => {};
+const noop = () => { };
 
 ViewportPane.defaultProps = {
   onInteraction: noop,
